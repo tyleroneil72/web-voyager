d724f8400ca967967a166f5db87df4b4ef967b73
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-15 21:42:10 -0500

    Remove comment

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,3 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-$(document).on("click", ".not-implemented", function () {
+﻿$(document).on("click", ".not-implemented", function () {
   alert("Not implemented yet");
 });
