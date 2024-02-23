e17d601a58e7cdcc56427e15ccbe382b1596ef06
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 19:23:03 -0500

    Update Spacing

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -6,21 +6,28 @@ public class Flight
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public required string Departure { get; set; }
+
     [Required]
     public required string Arrival { get; set; }
+
     [Required]
     [Display(Name = "Departure Time")]
     public required DateTime DepartureTime { get; set; }
+
     [Required]
     [Display(Name = "Arrival Time")]
     public required DateTime ArrivalTime { get; set; }
+
     [Required]
     public required string Airline { get; set; }
+
     [Required]
     [Display(Name = "Flight Number")]
     public required string FlightNumber { get; set; }
+
     [Required]
     public required string Status { get; set; }
 }
