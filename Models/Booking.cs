8971954c3aac78b0caaee74d91e5a38e853b7838
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-23 17:21:31 -0500

    Update Booking model

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -6,4 +6,11 @@ public class Booking
 {
     [Key]
     public int Id { get; set; }
+
+    [Required]
+    public required User User { get; set; }
+
+    public Flight? Flight { get; set; }
+    // public Hotel? Hotel { get; set; }
+    // public Car? Car { get; set; }
 }
