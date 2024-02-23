be14b985973dbbea3af5235773d0c8a43aa42d06
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 19:04:16 -0500

    Add AppDBContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using web_voyager.Models;
+
 namespace web_voyager.Controllers;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    public AppDbContext()
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
 
     }
