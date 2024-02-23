2e4b08a27bb004b8db5ad28d3ea424dfab5a8090
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 19:22:16 -0500

    Add Spacing

--- Models/User.cs
+++ Models/User.cs
@@ -7,16 +7,21 @@ public class User
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public required string Username { get; set; }
+
     [Required]
     public required string Password { get; set; }
+
     [Required]
     public required string Email { get; set; }
+
     [Required]
     public required string Role { get; set; }
+
     [Required]
     public required string Status { get; set; }
-    [Required]
-    public required List<Flight> Flights { get; set; }
+
+    public List<Flight>? Flights { get; set; }
 }
