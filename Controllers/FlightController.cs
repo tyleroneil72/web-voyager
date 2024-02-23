0368b2e5cd6efb820af8ee1474574b4996e18ac4
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 22:32:19 -0500

    Add Search for Flights

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -1,10 +1,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using web_voyager.Data;
 using web_voyager.Models;
 
 namespace web_voyager.Controllers;
-
+[Route("Flight")]
 public class FlightController : Controller
 {
     private readonly AppDbContext _db;
@@ -13,8 +14,29 @@ public class FlightController : Controller
     {
         _db = db;
     }
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View(_db.Flights.ToList());
+    }
+
+    [HttpGet("Search/{searchString?}")]
+    public async Task<IActionResult> Search(string searchString)
+    {
+        var flightsQuery = from f in _db.Flights
+                           select f;
+
+        bool searchPerformed = !String.IsNullOrEmpty(searchString);
+
+        if (searchPerformed)
+        {
+            // Assuming you want to search by both Departure and Arrival or any other fields
+            flightsQuery = flightsQuery.Where(f => f.Departure.Contains(searchString) || f.Arrival.Contains(searchString));
+        }
+        var flights = await flightsQuery.ToListAsync();
+        ViewData["searchPerformed"] = searchPerformed;
+        ViewData["searchString"] = searchString;
+
+        return View("Index", flights); // Make sure the view is expecting a list of flights
     }
 }
