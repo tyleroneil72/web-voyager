63b04cf8d71a4880544dd8e0e69a724a150f4e9f
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 23:29:41 -0500

    Add airline and flight number to search

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -31,7 +31,7 @@ public class FlightController : Controller
         if (searchPerformed)
         {
             // Assuming you want to search by both Departure and Arrival or any other fields
-            flightsQuery = flightsQuery.Where(f => f.Departure.Contains(searchString) || f.Arrival.Contains(searchString));
+            flightsQuery = flightsQuery.Where(f => f.Departure.Contains(searchString) || f.Arrival.Contains(searchString) || f.Airline.Contains(searchString) || f.FlightNumber.Contains(searchString));
         }
         var flights = await flightsQuery.ToListAsync();
         ViewData["searchPerformed"] = searchPerformed;
