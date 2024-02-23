8ef86727557036e705e3ead136f79e6b18680007
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-22 19:27:54 -0500

    Add DB Set

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,8 @@ namespace web_voyager.Controllers;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Flight> Flights { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
 
