bd261554c8e39585c6541fffe428d35474740458
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-24 22:00:04 -0500

    Create Details controller

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using web_voyager.Data;
 using web_voyager.Models;
 
@@ -17,5 +18,16 @@ public class CarController : Controller
     public IActionResult Index()
     {
         return View(_db.Cars.ToList());
+    }
+
+    [HttpGet("Details/{id:int}")]
+    public async Task<IActionResult> Details(int id)
+    {
+        var car = await _db.Hotels.FirstOrDefaultAsync(h => h.Id == id);
+        if (car == null)
+        {
+            return NotFound();
+        }
+        return View(car);
     }
 }
