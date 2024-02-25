8a352c471df62d04e5c6cf22380766b0ec6d8506
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-24 21:54:08 -0500

    Add Db Context to Controller

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -1,14 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using web_voyager.Data;
 using web_voyager.Models;
 
 namespace web_voyager.Controllers;
 [Route("Car")]
 public class CarController : Controller
 {
+    private readonly AppDbContext _db;
+
+    public CarController(AppDbContext db)
+    {
+        _db = db;
+    }
     [HttpGet("")]
     public IActionResult Index()
     {
-        return View();
+        return View(_db.Cars.ToList());
     }
 }
