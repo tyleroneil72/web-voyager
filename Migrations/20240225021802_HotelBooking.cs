761861afcaa5dac5a9f02dcf018d7e6f37b3db93
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-24 21:18:40 -0500

    Add Hotel Booking Migration

---
+++ Migrations/20240225021802_HotelBooking.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_voyager.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HotelId",
+                table: "Bookings",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_HotelId",
+                table: "Bookings",
+                column: "HotelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Hotels_HotelId",
+                table: "Bookings",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Hotels_HotelId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_HotelId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "HotelId",
+                table: "Bookings");
+        }
+    }
+}
