901b2c1e9ed830ce4a4786b1247fbd32f6752c7e
Tyler O'Neil <tyleroneildev@gmail.com>
2024-02-24 19:19:07 -0500

    Add Search Feature

--- Controllers/HotelController.cs
+++ Controllers/HotelController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using web_voyager.Data;
 using web_voyager.Models;
 
@@ -21,4 +22,26 @@ public class HotelController : Controller
     {
         return View(_db.Hotels.ToList());
     }
+
+    [HttpGet("Search/{searchString?}")]
+    public async Task<IActionResult> Search(string searchString)
+    {
+        var hotelsQuery = from h in _db.Hotels
+                          select h;
+
+        bool searchPerformed = !String.IsNullOrEmpty(searchString);
+
+        if (searchPerformed)
+        {
+            hotelsQuery = hotelsQuery.Where(h => h.Name.Contains(searchString) ||
+                                                 h.Location.Contains(searchString) ||
+                                                 h.Description.Contains(searchString));
+        }
+        var hotels = await hotelsQuery.ToListAsync();
+        ViewData["searchPerformed"] = searchPerformed;
+        ViewData["searchString"] = searchString;
+
+        return View("Index", hotels);
+    }
+
 }
