c579bde3e5a4b7337b473a9b680e67044e27a614
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-12 11:42:22 -0400

    Edit Email Sender Service template

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,7 +1,11 @@
-namespace web_voyager.Services
-{
-    public class EmailSender : IEmailSender
-    {
+using Microsoft.AspNetCore.Identity.UI.Services;
 
+namespace web_voyager.Services;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        throw new NotImplementedException();
     }
 }
