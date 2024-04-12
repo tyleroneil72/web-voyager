2480a6191ec63395525bc37a4449e7a9d5f9938f
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-11 23:26:06 -0400

    Change to User

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -21,7 +21,7 @@ public class AppDbContext : IdentityDbContext<User>
         base.OnModelCreating(builder);
         builder.HasDefaultSchema("Identity");
 
-        builder.Entity<IdentityUser>(entity =>
+        builder.Entity<User>(entity =>
         {
             entity.ToTable(name: "User");
         });
