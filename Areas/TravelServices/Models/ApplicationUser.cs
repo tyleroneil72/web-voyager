29a978f5c83240e3950f1314459a493070a38967
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-12 14:36:56 -0400

    Create Application User

---
+++ Areas/TravelServices/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace web_voyager.Areas.TravelServices.Models;
+
+public class ApplicationUser : IdentityUser
+{
+
+}
