f62ebb2918e3a79ebe424acc9f5cec07c008f786
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-11 23:24:25 -0400

    Add OnModelCreating Method

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,4 +15,45 @@ public class AppDbContext : IdentityDbContext<User>
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasDefaultSchema("Identity");
+
+        builder.Entity<User>(entity =>
+        {
+            entity.ToTable(name: "User");
+        });
+
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Role");
+        });
+
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
+    }
 }
