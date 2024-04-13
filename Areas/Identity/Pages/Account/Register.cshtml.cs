da877807dbb635c3175a66b79fa0fcade92e1778
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-13 19:04:11 -0400

    Add Role Defaulting to Traveller

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -136,6 +136,7 @@ namespace web_voyager.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, Enum.Roles.Traveller.ToString());
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
