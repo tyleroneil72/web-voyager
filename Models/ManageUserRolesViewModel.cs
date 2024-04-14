b32876e5ea16ffa8485c1271fb3f0fed416ae603
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-13 21:11:08 -0400

    Edit Formatting

--- Models/ManageUserRolesViewModel.cs
+++ Models/ManageUserRolesViewModel.cs
@@ -2,9 +2,7 @@ namespace web_voyager.Models;
 
 public class ManageUserRolesViewModel
 {
-
     public required string RolesId { get; set; }
     public required string RoleName { get; set; }
     public bool Selected { get; set; }
-
 }
--- Models/UserRolesViewModel.cs
+++ Models/UserRolesViewModel.cs
@@ -7,5 +7,4 @@ public class UserRolesViewModel
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required IEnumerable<string> Roles { get; set; }
-
 }
