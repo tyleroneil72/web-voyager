b6ce8423fa7ad8b785c99763a23788b0ccb37376
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-14 21:12:17 -0400

    Add Logging to all Car Controller Methods

--- Areas/TravelServices/Controllers/CarController.cs
+++ Areas/TravelServices/Controllers/CarController.cs
@@ -39,18 +39,28 @@ public class CarController : Controller
     [HttpGet("Details/{id:int}")]
     public async Task<IActionResult> Details(int id)
     {
-        var car = await _db.Cars.FirstOrDefaultAsync(h => h.Id == id);
-        if (car == null)
+        _logger.LogInformation("Car Details page visited.");
+        try
         {
-            return NotFound();
+            var car = await _db.Cars.FirstOrDefaultAsync(h => h.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
         }
-        return View(car);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpGet("Create")]
     [Authorize(Roles = "Admin")]
     public IActionResult Create()
     {
+        _logger.LogInformation("Car Create page visited.");
         return View();
     }
 
@@ -59,25 +69,43 @@ public class CarController : Controller
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Create(Car car)
     {
-        if (ModelState.IsValid)
+        _logger.LogInformation("Car Create Post page visited.");
+        try
         {
-            _db.Cars.Add(car);
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Cars.Add(car);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(car);
         }
-        return View(car);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpGet("Edit/{id:int}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Edit(int id)
     {
-        var car = await _db.Cars.FindAsync(id);
-        if (car == null)
+        _logger.LogInformation("Car Edit page visited.");
+        try
         {
-            return NotFound();
+            var car = await _db.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
         }
-        return View(car);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpPost("Edit/{id:int}")]
@@ -85,31 +113,41 @@ public class CarController : Controller
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Edit(int id, [Bind("Id,Brand,Model,Location,Description,PricePerDay,Seats,CarsAvailable")] Car car)
     {
-        if (id != car.Id)
+        _logger.LogInformation("Car Edit Post page visited.");
+
+        try
         {
-            return NotFound();
+            if (id != car.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(car);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarExists(car.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            return View(car);
         }
-        if (ModelState.IsValid)
+        catch (Exception e)
         {
-            try
-            {
-                _db.Update(car);
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CarExists(car.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            _logger.LogError(e.Message);
+            return View(null);
         }
-        return View(car);
     }
 
     private bool CarExists(int id)
@@ -121,12 +159,22 @@ public class CarController : Controller
     [Authorize(Roles = "Admin")]
     public IActionResult Delete(int id)
     {
-        var car = _db.Cars.FirstOrDefault(f => f.Id == id);
-        if (car == null)
+        _logger.LogInformation("Car Delete page visited.");
+
+        try
         {
-            return NotFound();
+            var car = _db.Cars.FirstOrDefault(f => f.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
         }
-        return View(car);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpPost("DeleteConfirmed/{id:int}"), ActionName("DeleteConfirmed")]
@@ -134,132 +182,180 @@ public class CarController : Controller
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
-        // First, find and delete any car bookings related to the car
-        var relatedBookings = _db.Bookings.Where(b => b.CarId == id).ToList();
-        if (relatedBookings.Any())
-        {
-            _db.Bookings.RemoveRange(relatedBookings);
-            await _db.SaveChangesAsync(); // Save changes after removing bookings
-        }
+        _logger.LogInformation("Car DeleteConfirmed page visited.");
 
-        // Then, find and delete the car
-        var car = await _db.Cars.FindAsync(id);
-        if (car != null)
+        try
         {
-            _db.Cars.Remove(car);
-            await _db.SaveChangesAsync(); // Save changes after removing the car
-            return RedirectToAction("Index");
-        }
+            // First, find and delete any car bookings related to the car
+            var relatedBookings = _db.Bookings.Where(b => b.CarId == id).ToList();
+            if (relatedBookings.Any())
+            {
+                _db.Bookings.RemoveRange(relatedBookings);
+                await _db.SaveChangesAsync(); // Save changes after removing bookings
+            }
 
-        return NotFound();
+            // Then, find and delete the car
+            var car = await _db.Cars.FindAsync(id);
+            if (car != null)
+            {
+                _db.Cars.Remove(car);
+                await _db.SaveChangesAsync(); // Save changes after removing the car
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpGet("Booking/{id:int}")]
     public async Task<IActionResult> Booking(int id)
     {
-        var car = await _db.Cars.FindAsync(id);
-        if (car == null)
+        _logger.LogInformation("Car Booking page visited.");
+        try
         {
-            return NotFound();
+            var car = await _db.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
         }
-        return View(car);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpPost("SubmitBooking/{id:int}")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> SubmitBooking(int id)
     {
-        // First, find the car based on the ID.
-        var car = await _db.Cars.FindAsync(id);
-        if (car == null)
-        {
-            return NotFound();
-        }
+        _logger.LogInformation("Car SubmitBooking Post page visited.");
 
-        if (User.Identity != null && User.Identity.IsAuthenticated)
+        try
         {
-            // Get the user's ID
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            // Check if the user exists in the database
-            var user = await _db.Users.FindAsync(userId);
-            if (user == null)
+            // First, find the car based on the ID.
+            var car = await _db.Cars.FindAsync(id);
+            if (car == null)
             {
-                // Handle the case where the user is not found. Could redirect to login or show an error.
+                return NotFound();
+            }
+
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                // Get the user's ID
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                // Check if the user exists in the database
+                var user = await _db.Users.FindAsync(userId);
+                if (user == null)
+                {
+                    // Handle the case where the user is not found. Could redirect to login or show an error.
+                    return NotFound("User not found.");
+                }
+                // Create a new booking object.
+                var newBooking = new Booking
+                {
+                    ApplicationUserId = userId, // Set the user's ID.
+                    ApplicationUser = user, // Set the user object.
+                    CarId = id, // Set the Cars ID
+                    Type = "Car",
+                };
+                car.CarsAvailable -= 1;
+                _db.Bookings.Add(newBooking);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("BookingConfirmation", new { id = newBooking.Id });
+            }
+
+
+            var guestUserId = 1; // Guest Id
+
+            var guestUser = await _db.GuestUsers.FindAsync(guestUserId);
+            if (guestUser == null)
+            {
                 return NotFound("User not found.");
             }
-            // Create a new booking object.
-            var newBooking = new Booking
+
+            var booking = new Booking
             {
-                ApplicationUserId = userId, // Set the user's ID.
-                ApplicationUser = user, // Set the user object.
-                CarId = id, // Set the Cars ID
+                GuestUserId = guestUserId,
+                GuestUser = guestUser,
+                CarId = id,
                 Type = "Car",
             };
+
             car.CarsAvailable -= 1;
-            _db.Bookings.Add(newBooking);
+            _db.Bookings.Add(booking);
             await _db.SaveChangesAsync();
 
-            return RedirectToAction("BookingConfirmation", new { id = newBooking.Id });
+            return RedirectToAction("BookingConfirmation", new { id = booking.Id });
         }
-
-
-        var guestUserId = 1; // Guest Id
-
-        var guestUser = await _db.GuestUsers.FindAsync(guestUserId);
-        if (guestUser == null)
+        catch (Exception e)
         {
-            return NotFound("User not found.");
+            _logger.LogError(e.Message);
+            return View(null);
         }
-
-        var booking = new Booking
-        {
-            GuestUserId = guestUserId,
-            GuestUser = guestUser,
-            CarId = id,
-            Type = "Car",
-        };
-
-        car.CarsAvailable -= 1;
-        _db.Bookings.Add(booking);
-        await _db.SaveChangesAsync();
-
-        return RedirectToAction("BookingConfirmation", new { id = booking.Id });
     }
 
     [HttpGet("BookingConfirmation/{id:int}")]
     public async Task<IActionResult> BookingConfirmation(int id)
     {
-        var booking = await _db.Bookings
-                                .Include(b => b.Car)
-                                .Include(b => b.GuestUser)
-                                .FirstOrDefaultAsync(b => b.Id == id);
-        if (booking == null)
+        _logger.LogInformation("Car BookingConfirmation page visited.");
+
+        try
         {
-            return NotFound();
+            var booking = await _db.Bookings
+                                    .Include(b => b.Car)
+                                    .Include(b => b.GuestUser)
+                                    .FirstOrDefaultAsync(b => b.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            return View(booking);
         }
-        return View(booking);
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
     [HttpGet("Search/{searchString?}")]
     public async Task<IActionResult> Search(string searchString)
     {
-        var carsQuery = from c in _db.Cars
-                        select c;
-
-        bool searchPerformed = !String.IsNullOrEmpty(searchString);
-
-        if (searchPerformed)
+        _logger.LogInformation("Car Search page visited.");
+        try
         {
-            carsQuery = carsQuery.Where(c => c.Brand.Contains(searchString) ||
-                                             c.Model.Contains(searchString) ||
-                                             c.Location.Contains(searchString) ||
-                                             c.Description.Contains(searchString));
-        }
-        var cars = await carsQuery.ToListAsync();
-        ViewData["searchPerformed"] = searchPerformed;
-        ViewData["searchString"] = searchString;
+            var carsQuery = from c in _db.Cars
+                            select c;
 
-        return View("Index", cars);
+            bool searchPerformed = !String.IsNullOrEmpty(searchString);
+
+            if (searchPerformed)
+            {
+                carsQuery = carsQuery.Where(c => c.Brand.Contains(searchString) ||
+                                                 c.Model.Contains(searchString) ||
+                                                 c.Location.Contains(searchString) ||
+                                                 c.Description.Contains(searchString));
+            }
+            var cars = await carsQuery.ToListAsync();
+            ViewData["searchPerformed"] = searchPerformed;
+            ViewData["searchString"] = searchString;
+
+            return View("Index", cars);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return View(null);
+        }
     }
 
 }
